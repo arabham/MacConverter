a9281cbf536326bc507e18d3b68bf94f86bf8499
Abraham Hoogendoorn <17817975+arabham@users.noreply.github.com>
2024-08-08 00:18:35 -0600

    Now auto puts converts into clipboard

--- MacConverter.cpp
+++ MacConverter.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <numeric>
+#include <algorithm>
+#include <windows.h>
 
 using namespace std;
 
@@ -18,6 +21,7 @@ int main()
     bool bRunAgain = false;
     do
     {
+        cout << "Enter as many macs as you would like converted then, press enter twice:" << endl;
         string mac;
         vector<string> macVectors;
         while (getline(cin, mac))
@@ -39,10 +43,12 @@ int main()
         vector<string> formattedMacs;
         for (it2 = strippedMacs.begin(); it2 != strippedMacs.end(); it2++)
         {
+            formattedMacs.push_back(*it2);
             formattedMacs.push_back(formatColon(*it2));
             formattedMacs.push_back(formatDot(*it2));
             formattedMacs.push_back(formatDash(*it2));
-            cout << *it << '\n';
+            formattedMacs.push_back("");
+            // cout << *it2 << '\n';
         }
 
         vector<string>::iterator it3;
@@ -50,6 +56,33 @@ int main()
         {
             cout << *it3 << '\n';
         }
+
+        // auto function to insert newlines between elements in vector
+        auto newLineFold = [](string a, string b)
+        {
+            return move(a) + '\n' + b;
+        };
+        
+        // makes the input vector into a string for conversion to c string
+        string macStrings = accumulate(next(formattedMacs.begin()), formattedMacs.end(), formattedMacs[0], newLineFold);
+        const char* macChars = macStrings.c_str();
+
+        // saves c string to clipboard
+        if (OpenClipboard(NULL))
+        {
+            EmptyClipboard();
+            HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, strlen(macChars) + 1);
+            if (hMem)
+            {            
+                char* pMem = (char*)GlobalLock(hMem);
+                strcpy_s(pMem, strlen(macChars) + 1, macChars);
+                GlobalUnlock(hMem);
+                SetClipboardData(CF_TEXT, hMem);
+            }
+            CloseClipboard();
+        }
+
+        cout << "All the conversions have been output to the console, and in your copy buffer." << endl;
         bRunAgain = AskToRunAgain();
     } while (bRunAgain);
     return 0;
